7da8dc82271f322d3edd7432bf0645b11b16a4f4
Tomo Hung <tomohung@gmail.com>
2016-06-28 22:01:57 +0800

    complete acronym

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -5,6 +5,10 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    
+    string
+    |> String.split(~r/[\s,-]/)
+    |> Enum.map(fn(x) -> if Regex.match?(~r/[A-Z]+/, x), do: x, else: String.capitalize(x) end)
+    |> Enum.map(fn(x) -> String.split(x, ~r/[a-z\s]/, trim: true) end)
+    |> List.to_string
   end
 end
